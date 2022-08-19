d898e9b729744b28fe567e8a870d281b45fee542
guilhermeamorim <guilhermeamorim@deso-se.com.br>
2022-08-19 14:13:24 -0300

    first commit

---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
+export class CreateUserUseCase {
+    constructor (
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider
+    ) {}
+
+    async execute(data: ICreateUserRequestDTO) {
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists.");
+        }
+
+        const user = new User(data);
+
+        await this.usersRepository.save(user);
+
+        this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email
+            },
+            from: {
+                name: "Guilherme Amorim",
+                email: "guilherme.o.a.ufal@gmail.com"
+            },
+            subject: "Olá!",
+            body: "Seja bem-vindo!"
+        })
+    }
+}
